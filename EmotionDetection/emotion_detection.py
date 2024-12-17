25f166726c06066b2ff158d15091c03df50b8e70
archbaer <pereirakebaili@gmail.com>
2024-12-17 08:08:43 -0500

    finished project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,15 +7,20 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse} }
 
     response = requests.post(url, json= myobj, headers=header, timeout=30)
-    
-    # Parsing the JSON response from the API
-    formatted_response = json.loads(response.text)
 
-    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    if response.status_code == 200:
+        try:
+            formatted_response = response.json()
+
+            anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+            disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+            fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+            joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+            sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        except:
+            anger = disgust = fear = joy = sadness = None
+    elif response.status_code > 200:
+        anger = disgust = fear = joy = sadness = None
 
     emotions = {
         "anger": anger,
@@ -27,7 +32,7 @@ def emotion_detector(text_to_analyse):
 
     dominant_emotion = None
     for emotion, score in emotions.items():
-        if score >= 0.6:
+        if score and score >= 0.6:
             dominant_emotion = emotion
             break
 
@@ -35,12 +40,5 @@ def emotion_detector(text_to_analyse):
         "emotions": emotions,
         "dominant_emotion": dominant_emotion
     }
-
-    print(result)
     
     return result
-
-
-# from emotion_detection import emotion_detector
-# emotion_detector("i am happy")
-# emotion_detector("I am sad")
--- server.py
+++ server.py
@@ -1,26 +1,31 @@
+"""Flask web server for emotion detection application."""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route("/emotionDetector")
-def emotion_detec():
+def emotion_detect():
+    """Handle emotion detection requests and return the system's response."""
     text_to_analyze = request.args.get("textToAnalyze")
-
     response = emotion_detector(text_to_analyze)
 
     emotions = response['emotions']
     dominant = response['dominant_emotion']
 
-    if emotions is None:
+    if dominant is None:
         return "Invalid input! Try again."
-    else:
-        return "For the given statement, the system response is {}. The dominant emotion is {}".format(emotions, dominant)
-
+    return (
+        f"For the given statement, the system response is {emotions}. "
+        f"The dominant emotion is {dominant}"
+    )
 
 @app.route("/")
 def render_index():
+    """Render the index.html page."""
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
+    
